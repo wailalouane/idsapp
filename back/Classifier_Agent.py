d69c8a8f24b379cd9b9879f5381876d5a11a4e88
GOBLOO <56554238+gobloo@users.noreply.github.com>
2023-02-04 10:28:35 +0100

    Update Classifier_Agent.py

--- back/Classifier_Agent.py
+++ back/Classifier_Agent.py
@@ -1,23 +1,39 @@
 import pickle
 import os ,sys
 import numpy as np
-def Classification(paquet) :
-    x=np.array([paquet])
-    pred_dt= pickled_model.predict(x)
-    print(pred_dt[0])
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
 
+class ClassificationAgent(Agent):
+    def setup(self):
+        print(f"Classification agent {self.jid} starting")
+        
+        # Load the pickled model
+        path = os.path.join(sys.path[0])
+        self.pickled_model = pickle.load(open(path + '../model.pkl', 'rb'))
 
-path = os.path.join(sys.path[0])
-pickled_model = pickle.load(open(path+'../model.pkl', 'rb'))
+        # Add a behaviour to receive and process incoming messages
+        classify_template = self.set_template("classify")
+        classify_behaviour = self.ClassifyBehaviour()
+        self.add_behaviour(classify_behaviour, classify_template)
 
+    def classify(self, paquet):
+        x = np.array([paquet])
+        pred_dt = self.pickled_model.predict(x)
+        return pred_dt[0]
 
-#just for test
-test = pd.read_csv('../test_validation.csv')
-test.drop(['Unnamed: 0'], axis=1, inplace=True)
+    class ClassifyBehaviour(CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive(timeout=10)
+            if msg:
+                # Get the data from the message
+                paquet = msg.body
 
+                # Use the classification function to make a prediction
+                prediction = self.agent.classify(paquet)
 
-i=0
-while i < len(test):
-    pred=Classification(test.iloc[i])
-    print(pred)
-    i += 1
+                # Send the prediction back to the sender
+                reply = Message(to=msg.sender)
+                reply.body = prediction
+                await self.send(reply)
